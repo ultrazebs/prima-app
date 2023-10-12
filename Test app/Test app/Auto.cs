a1d1887d560ca8cf0d5eb8732475c12624eb24aa
Ultrazebs <riki_zottoli@hotmail.it>
2023-10-12 17:22:19 +0200

    Moved class Auto in other file for better management

---
+++ Test app/Test app/Auto.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace testApp;
+
+class Auto
+{
+    //tipi personalizzati
+    public enum motore
+    {
+        benzina,
+        diesel,
+        elettrico
+    }
+    //proprietà
+    public string marca;
+    public string modello;
+    public string colore;
+    public motore tipoMotore;
+    /// <summary>
+    /// true if on - false if off
+    /// </summary>
+    private bool engine;
+    public int fuelLevel;
+
+
+
+    /// <summary>
+    /// stampa la descrizione del veicolo
+    /// </summary>
+    public void stampaDescrizione()
+    {
+        Console.WriteLine(GeneraDescrizione());
+    }
+
+
+
+    /// <summary>
+    /// stampa lo stato attuale del veicolo
+    /// </summary>
+    public void stampaStato()
+    {
+
+    }
+
+
+    public string generaStato()
+    {
+        string result;
+        result = $"Carburante: {this.fuelLevel}";
+        if (this.engine)
+            result += " accesa";
+        else
+            result += "spenta";
+
+        return result;
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public string GeneraDescrizione()
+    {
+        // dichiarazione variabili
+        string result;
+
+        // costruisco il risultato concatenando le descrizioni
+        result = "marca: " + this.marca + "\n";
+        result += $"modello:  {this.modello}\n";
+        result += "colore: " + this.colore + " \n";
+        result += "tipo motore: " + this.tipoMotore + "\n";
+        result += "engine: " + this.engine + "\n";
+        result += "fuel level: " + this.fuelLevel + "\n";
+        result += "\n_______________________________\n";
+
+        // restituisco il risultato
+        return result;
+    }
+
+
+
+    /// <summary>
+    ///Costruttore con argomenti per la classe Auto che valorizza le proprietà utilizzando i valori passati come argomento
+    /// </summary>
+    public Auto(string marca, string modello, string colore, motore tipoMotore, int fuelLevel)
+    {
+        this.marca = marca;
+        this.modello = modello;
+        this.colore = colore;
+        this.tipoMotore = tipoMotore;
+        this.fuelLevel = fuelLevel;
+    }
+
+
+    public bool getEngineStatus()
+    {
+        return engine;
+    }
+
+
+
+    /// <summary>
+    /// metodo che setta il valore di engine a true
+    /// </summary>
+    public void accendi()
+    {
+        this.engine = true;
+    }
+
+
+
+    /// <summary>
+    /// metodo che setta il valore di engine a false
+    /// </summary>
+    public void spegni()
+    {
+        this.engine = false;
+    }
+}
+
+// end of class Auto
--- Test app/Test app/Program.cs
+++ Test app/Test app/Program.cs
@@ -10,12 +10,13 @@
  * 
  * 
  */
-Auto miaAuto;
+
+testApp.Auto miaAuto;
 
 Console.WriteLine("On a scale from 0 to 100, how much fuel do you have in your tank?");
 int bomber0 = Convert.ToInt32(Console.ReadLine());
 
-miaAuto = new Auto("Bugatti", "Chiron Pur Sport", "Bronzo", Auto.motore.benzina, bomber0);
+miaAuto = new testApp.Auto("Bugatti", "Chiron Pur Sport", "Bronzo", testApp.Auto.motore.benzina, bomber0);
 
 Console.WriteLine("Do you want to turn on your car (I'll turn it on only if the fuel level is not 0)?");
 string bomber = Console.ReadLine();
@@ -44,119 +45,3 @@ else if (miaAuto.fuelLevel != 0 && bomber1 == "no")
     Console.WriteLine("Car is still running bro!");
 
 
-class Auto
-{
-    //tipi personalizzati
-    public enum motore
-    {
-        benzina,
-        diesel,
-        elettrico
-    }
-    //proprietà
-    public string marca;
-    public string modello;
-    public string colore;
-    public motore tipoMotore;
-    /// <summary>
-    /// true if on - false if off
-    /// </summary>
-    private bool engine;
-    public int fuelLevel;
-
-
-
-    /// <summary>
-    /// stampa la descrizione del veicolo
-    /// </summary>
-    public void stampaDescrizione()
-    {
-        Console.WriteLine(GeneraDescrizione());
-    }
-
-
-
-    /// <summary>
-    /// stampa lo stato attuale del veicolo
-    /// </summary>
-    public void stampaStato()
-    {
-
-    }
-
-
-    public string generaStato()
-    {
-        string result;
-        result = $"Carburante: {this.fuelLevel}";
-        if (this.engine)
-            result += " accesa";
-        else
-            result += "spenta";
-
-        return result;
-    }
-
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <returns></returns>
-    public string GeneraDescrizione()
-    {
-        // dichiarazione variabili
-        string result;
-
-        // costruisco il risultato concatenando le descrizioni
-        result = "marca: " + this.marca + "\n";
-        result += $"modello:  {this.modello}\n";
-        result += "colore: " + this.colore + " \n";
-        result += "tipo motore: " + this.tipoMotore + "\n";
-        result += "engine: " + this.engine + "\n";
-        result += "fuel level: " + this.fuelLevel + "\n";
-        result += "\n_______________________________\n";
-
-        // restituisco il risultato
-        return result;
-    }
-
-
-
-    /// <summary>
-    ///Costruttore con argomenti per la classe Auto che valorizza le proprietà utilizzando i valori passati come argomento
-    /// </summary>
-    public Auto(string marca, string modello, string colore, motore tipoMotore, int fuelLevel)
-    {
-        this.marca = marca;
-        this.modello = modello;
-        this.colore = colore;
-        this.tipoMotore = tipoMotore;
-        this.fuelLevel = fuelLevel;
-    }
-
-
-    public bool getEngineStatus()
-    {
-        return engine;
-    }
-    
-    
-    
-    /// <summary>
-    /// metodo che setta il valore di engine a true
-    /// </summary>
-    public void accendi()
-    {
-        this.engine = true;
-    }
-    
-    
-    
-    /// <summary>
-    /// metodo che setta il valore di engine a false
-    /// </summary>
-    public void spegni()
-    {
-        this.engine = false;
-    }
-}
